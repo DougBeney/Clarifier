017fce68b186b0434fabffe5891d3245c3c1fa9a
Doug Beney <work@dougie.io>
2018-07-19 20:33:50 -0400

    more features

--- csvlang.py
+++ csvlang.py
@@ -5,17 +5,25 @@ import string
 import os.path
 import re
 
+working_input_file = ""
+working_output_file = ""
+working_sheet = None
+working_line_number = 0
+working_row_indexes_to_delete = []
+working_col_indexes_to_delete = []
+
 if not os.path.isfile("config.csv"):
 	print("You must have a config.csv file set up.")
 	exit()
 
+config = p.get_sheet(file_name="config.csv")
+config_sheet = config.get_array()
+
 def err(*messages, fatal=False):
 	err_label = "ERROR:" if not fatal else "FATAL:"
-	print(err_label, " ".join(messages))
+	print(err_label, " ".join(messages), "[Line #"+str(working_line_number)+"]")
 	if fatal:
-		print("Exiting due to the last fatal error...")
-		exit()
-
+		print("Exiting due to the last fatal error...") exit()
 # https://stackoverflow.com/a/12640614
 def col2num(col):
 	num = 0
@@ -29,32 +37,74 @@ def col2num(col):
 def sc(str):
 	return str.lstrip().rstrip()
 
-working_input_file = ""
-working_output_file = ""
-working_sheet = None
-config = p.get_sheet(file_name="config.csv")
-indexes_to_delete = []
-
-config_sheet = config.get_array()
+def execute_and_save():
+	for index in sorted(working_col_indexes_to_delete, reverse=True):
+		del working_sheet.row[index]
+	for index in sorted(working_row_indexes_to_delete, reverse=True):
+		del working_sheet.col[index]
+	if (working_output_file == "~print~"):
+		print(working_sheet)
+	else:
+		working_sheet.save_as(working_output_file)
+	working_row_indexes_to_delete = []
+	working_col_indexes_to_delete = []
 
 for row_index,row in enumerate(config_sheet):
-	value = sc(row[0].upper())
-	if value == "FILE":
+	OPERATION = sc(row[0].upper())
+	working_line_number = row_index + 1
+	if OPERATION == "FILE":
 		if (len(row) >= 2):
+			if working_sheet is not None:
+				execute_and_save()
 			working_input_file = sc(row[1])
 			working_output_file = sc(row[2])
 			working_sheet = p.get_sheet(file_name=working_input_file)
 			print("FILE:", working_input_file)
 		else:
 			err("FILE calls must include an input and output file.", fatal=True)
-	elif value == "ROW":
-		err("Row processing is not implemented yet.")
-	elif value == "COL":
-		if (working_sheet):
-			print("hi")
-		else:
-			err("You are trying to do a", value, "operation when a sheet is not loaded.", fatal=True)
+	elif working_sheet:
+		elif OPERATION == "ROW" or OPERATION == "COL":
+			# 0        1    2         3
+			# ROW/COL, 1/a, operator, search
+			if (len(row) < 4):
+				err(OPERATION, "operation requires 3 additional arguments", fatal=True)
+			sheet_array         = None
+			location            = row[1]
+			search_operator     = row[2]
+			search_str          = row[3]
+			working_sheet_index = 0
 
+			if   OPERATION == "ROW":
+				# get col array so we can easily check a row
+				sheet_array = working_sheet.col
+			elif OPERATION == "COL":
+				# get row array so we can easily check a col
+				sheet_array = working_sheet.row
+				# must convert col letter to number
+				location = col2num(location)
+
+			def DeleteIfNeeded(op_condition, should_keep):
+				global OPERATION
+				if search_operator == op_condition and not should_keep:
+					print("Deleting", OPERATION, working_sheet_index+1)
+					if OPERATION == "ROW":
+						working_row_indexes_to_delete.append([OPERATION, working_sheet_index])
+					elif OPERATION == "COL":
+						working_col_indexes_to_delete.append([OPERATION, working_sheet_index])
+
+			for sheet_index,row_or_col in enumerate(working_sheet.row):
+				cell_value = row_or_col[location]
+				working_sheet_index = sheet_index
+				DeleteIfNeeded("contains"    , search_str in cell_value)
+				DeleteIfNeeded("!contains"   , search_str not in cell_value)
+				DeleteIfNeeded("="           , search_str == cell_value)
+				DeleteIfNeeded("!="          , search_str != cell_value)
+				DeleteIfNeeded("regcontains" , re.search(search_str, cell_value))
+				DeleteIfNeeded("!regcontains", not re.search(search_str, cell_value))
+	else
+		err("You are trying to do a", value, "operation when a sheet is not loaded.", fatal=True)
+
+execute_and_save()
 
 # for sheet_index,row in enumerate(sheet.row):
 # 	for i in range(1, len(config_sheet)):
