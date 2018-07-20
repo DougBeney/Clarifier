d8b0fe10df4b0be2de2da6ecb5754a08a20aa9bf
Dog <contact@dougie.io>
2018-07-20 00:18:11 -0400

    Working!

--- csvlang.py
+++ csvlang.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import pyexcel as p
 import string
 import os.path
@@ -9,6 +8,7 @@ working_input_file = ""
 working_output_file = ""
 working_sheet = None
 working_line_number = 0
+working_cell_coord = [0,0]
 working_row_indexes_to_delete = []
 working_col_indexes_to_delete = []
 
@@ -20,10 +20,14 @@ config = p.get_sheet(file_name="config.csv")
 config_sheet = config.get_array()
 
 def err(*messages, fatal=False):
-	err_label = "ERROR:" if not fatal else "FATAL:"
-	print(err_label, " ".join(messages), "[Line #"+str(working_line_number)+"]")
+	err_label  = "ERROR:" if not fatal else "FATAL:"
+	linum_str  = "[Line #"+str(working_line_number)+" in config.csv]"
+	cell_linum_str = "[Sheet Coord: "+num2col(working_cell_coord[0]+1)+str(working_cell_coord[1]+1)+"]"
+	print(err_label, linum_str, cell_linum_str, " ".join(messages))
 	if fatal:
-		print("Exiting due to the last fatal error...") exit()
+		print("Exiting due to the last fatal error...")
+		exit()
+
 # https://stackoverflow.com/a/12640614
 def col2num(col):
 	num = 0
@@ -32,16 +36,40 @@ def col2num(col):
 			num = num * 26 + (ord(c.upper()) - ord('A')) + 1
 	return num-1
 
+# https://stackoverflow.com/a/23862195
+def num2col(num):
+	string = ""
+	while num > 0:
+		num, remainder = divmod(num - 1, 26)
+		string = chr(65 + remainder) + string
+	return string
+
+# https://stackoverflow.com/a/6330109
+def safe_cast(val, to_type, default=None):
+	if str(val) == "":
+		return 0
+	try:
+		return to_type(val)
+	except (ValueError, TypeError):
+		err("You have entered a DISGUSTING value [", '"'+ sc(val)+'"', "]", fatal=True)
+
+def safe_num(val):
+	if ("." in str(val)):
+		return safe_cast(val, int)
+	else:
+		return safe_cast(val, float)
+
 # Stands for "String Clean"
-# Removes loading and trailing whitespace
-def sc(str):
-	return str.lstrip().rstrip()
+# Removes leading and trailing whitespace
+def sc(your_string):
+	return str(your_string).lstrip().rstrip()
 
 def execute_and_save():
+	global working_col_indexes_to_delete, working_row_indexes_to_delete
 	for index in sorted(working_col_indexes_to_delete, reverse=True):
 		del working_sheet.row[index]
 	for index in sorted(working_row_indexes_to_delete, reverse=True):
-		del working_sheet.col[index]
+		del working_sheet.column[index]
 	if (working_output_file == "~print~"):
 		print(working_sheet)
 	else:
@@ -63,74 +91,91 @@ for row_index,row in enumerate(config_sheet):
 		else:
 			err("FILE calls must include an input and output file.", fatal=True)
 	elif working_sheet:
-		elif OPERATION == "ROW" or OPERATION == "COL":
+		if OPERATION == "ROW" or OPERATION == "COL":
 			# 0        1    2         3
 			# ROW/COL, 1/a, operator, search
 			if (len(row) < 4):
 				err(OPERATION, "operation requires 3 additional arguments", fatal=True)
 			sheet_array         = None
-			location            = row[1]
-			search_operator     = row[2]
-			search_str          = row[3]
+			location            = sc(row[1])
+			location_array      = []        # If one or two ':' are in 'location' - to define start and end points
+			search_operator     = sc(row[2])
+			search_str          = sc(row[3])
 			working_sheet_index = 0
 
-			if   OPERATION == "ROW":
+			def loc_len():
+				return len(location_array)
+
+			if(":" in location):
+				location_array = list(filter(None, location.split(":")))
+				if loc_len() > 1:
+					location = location_array[0]
+				else:
+					err("Your location parameter is screwed up. Ask a doctor to check out your colons :)", fatal=True)
+
+			if OPERATION == "ROW":
 				# get col array so we can easily check a row
-				sheet_array = working_sheet.col
-			elif OPERATION == "COL":
+				sheet_array = working_sheet.column
+				location = int(location)-1
+				if loc_len() > 1:
+					location_array[1] = col2num(location_array[1])-1
+				if loc_len() > 2:
+					if location_array[2] == "~":
+						location_array[2] = len(working_sheet.array[0]) # Column count
+					else:
+						location_array[2] = col2num(location_array[2])-1
+			else:
 				# get row array so we can easily check a col
 				sheet_array = working_sheet.row
 				# must convert col letter to number
 				location = col2num(location)
+				if loc_len() > 1:
+					location_array[1] = int(location_array[1])-1
+					print(location_array)
+				if loc_len() > 2:
+					if location_array[2] == "~":
+						location_array[2] = len(working_sheet.array) # Row count
+						print(len(working_sheet.array))
+					else:
+						location_array[2] = int(location_array[2])-1
 
 			def DeleteIfNeeded(op_condition, should_keep):
-				global OPERATION
-				if search_operator == op_condition and not should_keep:
-					print("Deleting", OPERATION, working_sheet_index+1)
-					if OPERATION == "ROW":
-						working_row_indexes_to_delete.append([OPERATION, working_sheet_index])
-					elif OPERATION == "COL":
-						working_col_indexes_to_delete.append([OPERATION, working_sheet_index])
+				force_true_lambda = lambda x,y: True
+				if loc_len() > 1:
+					should_keep = force_true_lambda if working_sheet_index < location_array[1] else should_keep
+					if loc_len() > 2:
+						should_keep = force_true_lambda if working_sheet_index > location_array[2] else should_keep
+				if search_operator == op_condition:
+					if not should_keep(search_str, cell_value):
+						if OPERATION == "ROW":
+							print(search_str, cell_value, should_keep(search_str, cell_value))
+							working_row_indexes_to_delete.append(working_sheet_index)
+						elif OPERATION == "COL":
+							working_col_indexes_to_delete.append(working_sheet_index)
 
-			for sheet_index,row_or_col in enumerate(working_sheet.row):
-				cell_value = row_or_col[location]
+			for sheet_index,row_or_col in enumerate(sheet_array):
+				cell_value = str(row_or_col[location])
+				if OPERATION == "ROW":
+					working_cell_coord[1] = location
+					working_cell_coord[0] = sheet_index
+				else:
+					working_cell_coord[1] = sheet_index
+					working_cell_coord[0] = location
 				working_sheet_index = sheet_index
-				DeleteIfNeeded("contains"    , search_str in cell_value)
-				DeleteIfNeeded("!contains"   , search_str not in cell_value)
-				DeleteIfNeeded("="           , search_str == cell_value)
-				DeleteIfNeeded("!="          , search_str != cell_value)
-				DeleteIfNeeded("regcontains" , re.search(search_str, cell_value))
-				DeleteIfNeeded("!regcontains", not re.search(search_str, cell_value))
-	else
+				# Defining the Syntax
+				# x=search_str y=cell_value
+				DeleteIfNeeded("is"           , lambda x,y: x == y)
+				DeleteIfNeeded("!is"          , lambda x,y: x != y)
+				DeleteIfNeeded("contains"     , lambda x,y: x in y)
+				DeleteIfNeeded("!contains"    , lambda x,y: x not in y)
+				DeleteIfNeeded("regcontains"  , lambda x,y: re.search(x, y))
+				DeleteIfNeeded("!regcontains" , lambda x,y: not re.search(x, y))
+				DeleteIfNeeded(">"            , lambda x,y: safe_num(y) > safe_num(x))
+				DeleteIfNeeded(">="           , lambda x,y: safe_num(y) >= safe_num(x))
+				DeleteIfNeeded("<"            , lambda x,y: safe_num(y) < safe_num(x))
+				DeleteIfNeeded("<="           , lambda x,y: safe_num(y) <= safe_num(x))
+	else:
 		err("You are trying to do a", value, "operation when a sheet is not loaded.", fatal=True)
 
 execute_and_save()
 
-# for sheet_index,row in enumerate(sheet.row):
-# 	for i in range(1, len(config_sheet)):
-# 		search_col = col2num(config_sheet[i][0])
-# 		search_operation = config_sheet[i][1]
-# 		search_str = config_sheet[i][2]
-
-# 		cell_value = row[search_col]
-
-# 		def DeleteIfNeeded(op_condition, should_keep):
-# 			if search_operation == op_condition and not should_keep:
-# 				print("Deleting row", sheet_index+1)
-# 				indexes_to_delete.append(sheet_index)
-
-# 		DeleteIfNeeded("contains" ,   search_str in cell_value)
-# 		DeleteIfNeeded("!contains",   search_str not in cell_value)
-# 		DeleteIfNeeded("="        ,   search_str == cell_value)
-# 		DeleteIfNeeded("!="       ,   search_str != cell_value)
-# 		DeleteIfNeeded("regcontains", re.search(search_str, cell_value))
-# 		DeleteIfNeeded("!regcontains", not re.search(search_str, cell_value))
-
-# for index in sorted(indexes_to_delete, reverse=True):
-# 	del sheet.row[index]
-
-# save_dest = config.row[0][1]
-# if (save_dest == "~print~"):
-# 	print(sheet)
-# else:
-#   sheet.save_as(save_dest)
